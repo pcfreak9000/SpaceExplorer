b490b0b018b011379a274f7a0495163563458797
pcfreak9000 <roman.borris@gmail.com>
2020-04-06 01:03:30 +0200

    fixed GUI

--- api_src/de/pcfreak9000/space/core/LoadingScreen.java
+++ api_src/de/pcfreak9000/space/core/LoadingScreen.java
@@ -11,52 +11,52 @@ import de.omnikryptec.resource.Font;
 import de.omnikryptec.util.Logger;
 
 public class LoadingScreen {
-
-    private static final boolean DEBUG = false;
-
+    
+    private static final boolean DEBUG = true;
+    
     public static class LoadingEvent extends Event {
         private final String name;
         private final boolean enableSecondBar;
-
+        
         public LoadingEvent(String name) {
             this.name = name;
             this.enableSecondBar = false;
         }
-
+        
         public LoadingEvent(String name, boolean b) {
             this.name = name;
             this.enableSecondBar = b;
         }
     }
-
+    
     public static class LoadingSubEvent extends Event {
         private final String name;
         private final int number;
         private final int max;
-
+        
         public LoadingSubEvent(String name, int number, int max) {
             this.name = name;
             this.number = number;
             this.max = max;
         }
     }
-
+    
     private static final Logger LOGGER = Logger.getLogger(LoadingScreen.class);
-
+    
     public static final EventBus LOADING_STAGE_BUS = new EventBus();
-
+    
     private final GuiComponent component;
-
+    
     private final GuiImage background;
-
+    
     private final GuiProgressBar bar0;
     private final GuiProgressBar bar1;
-
+    
     private final int max;
     private int bar0count = 0;
-
+    
     private boolean active = false;
-
+    
     public LoadingScreen(int max) {
         LOGGER.debugf("Creating LoadingScreen (Max stages: %d)", max);
         LOADING_STAGE_BUS.register(this);
@@ -64,42 +64,39 @@ public class LoadingScreen {
         Font font = Omnikryptec.getFontsS().getFontSDF("candara");
         this.background = new GuiImage();
         this.background.setTexture(Omnikryptec.getTexturesS().get("hyperraum.png"));
-        this.background.setMaxAlways(true);
         this.bar0 = new GuiProgressBar();
-        this.bar0.setPos(0.1f, 0.5f);
-        this.bar0.setSize(0.8f, 0.1f);
+        this.bar0.setDimensions(0.1f, 0.5f, 0.8f, 0.1f);
         this.bar0.setFont(font);
         this.bar0.colorEmpty().set(1, 0, 0);
         this.bar0.colorFull().set(0, 1, 0);
         this.bar0.setText("Loading...");
         this.bar1 = new GuiProgressBar();
-        this.bar1.setPos(0.1f, 0.3f);
-        this.bar1.setSize(0.8f, 0.1f);
+        this.bar1.setDimensions(0.1f, 0.3f, 0.8f, 0.1f);
         this.bar1.setFont(font);
         this.bar1.colorEmpty().set(1, 0, 0);
         this.bar1.colorFull().set(0, 1, 0);
-        this.bar1.setEnabled(false);
+        this.bar1.setVisibility(false);
         this.component = new GuiComponent();
         this.component.addComponent(this.background);
         this.component.addComponent(this.bar0);
         this.component.addComponent(this.bar1);
     }
-
+    
     public void begin() {
         LOGGER.debug("Starting LoadingScreen");
         this.active = true;
         Omnikryptec.getGameS().getGuiManager().setGui(this.component);
     }
-
+    
     public void end() {
         LOADING_STAGE_BUS.unregister(this);
         Omnikryptec.getGameS().getGuiManager().setGui(null);
         this.active = false;
         LOGGER.debug("Ended LoadingScreen");
     }
-
+    
     private long lastupdate = 0;
-
+    
     private void checkUpdate() {
         if (!this.active) {
             return;
@@ -117,7 +114,7 @@ public class LoadingScreen {
             }
         }
     }
-
+    
     @EventSubscription
     public void onLoadingEvent(LoadingEvent ev) {
         this.bar0count++;
@@ -127,10 +124,10 @@ public class LoadingScreen {
             this.bar1.setValue(0);//?
             this.bar1.setText("Loading...");
         }
-        this.bar1.setEnabled(ev.enableSecondBar);
+        this.bar1.setVisibility(ev.enableSecondBar);
         checkUpdate();
     }
-
+    
     @EventSubscription
     public void onLoadingSubEvent(LoadingSubEvent ev) {
         this.bar1.setValue(ev.number / (float) ev.max);
